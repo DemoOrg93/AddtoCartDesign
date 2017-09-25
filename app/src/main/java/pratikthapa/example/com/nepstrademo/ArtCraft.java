f0499897eec32bf5b8cd1c539b0678fa68f634fc
Prakriti <prakritiadhikari2@gmail.com>
2017-09-25 12:22:13 +0545

    Pojo

---
+++ app/src/main/java/pratikthapa/example/com/nepstrademo/ArtCraft.java
@@ -0,0 +1,22 @@
+package pratikthapa.example.com.nepstrademo;
+
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarTab;
+import com.roughike.bottombar.OnTabSelectListener;
+
+public class ArtCraft extends AppCompatActivity {
+    BottomBar bottomBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art_craft);
+
+
+
+    }}
