0380a16935893f06444688b446a3f066bfd61273
pratik93 <31034554+pratik93@users.noreply.github.com>
2017-10-12 16:36:57 +0545

    new designs added

---
+++ app/src/main/java/pratikthapa/example/com/nepstrademo/ActivityProductDetail.java
@@ -0,0 +1,16 @@
+package pratikthapa.example.com.nepstrademo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pratikthapa on 10/12/17.
+ */
+
+public class ActivityProductDetail extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product_details);
+    }
+}
